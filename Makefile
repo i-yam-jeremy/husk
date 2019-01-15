31d996a6d14f162327e5edfe4154662ec237968f
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2019-01-15 11:31:01 -0800

    Successful disk read

--- Makefile
+++ Makefile
@@ -4,7 +4,11 @@ CC=gcc
 TARGET=husk.iso
 
 $(TARGET) : src/bootloader/bootloader.bin
+	mkisofs -o husk.iso src
+	dd conv=notrunc if=src/bootloader/bootloader.bin of=husk.iso
 
+run: $(TARGET)
+	qemu-system-i386 $(TARGET)
 
 src/bootloader/bootloader.bin: src/bootloader/bootloader.asm
 	$(NASM) -O0 -f bin -o src/bootloader/bootloader.bin src/bootloader/bootloader.asm
--- src/bootloader/bootloader.asm
+++ src/bootloader/bootloader.asm
@@ -1,18 +1,37 @@
-;
-; A simple  boot  sector  that  prints a message  to the  screen  using a BIOS  routine.
-;
-  mov ah, 0x0e   ; int  10/ah = 0eh -> scrolling  teletype  BIOS  routine
-  mov al, 48
-  int 0x10
-  mov al, 49
-  int 0x10
-  mov al, 50
-  int 0x10
+[org 0x7c00]
+
+  mov ah, 0x0e
   mov al, 51
   int 0x10
-  mov al, 52
+
+  mov dh, 1
+
+  mov bx, 0x9000
+
+  mov ah, 0x02     ; BIOS  read  sector  function
+  mov al, dh       ; Read DH  sectors
+  mov ch, 0x00     ; Select  cylinder 0
+  mov dh, 0x00     ; Select  head 0
+  mov cl, 0x02     ; Start  reading  from  second  sector (i.e.
+  ; after  the  boot  sector)
+  int 0x13          ; BIOS  interrupt
+  jc  disk_error    ; Jump if error (i.e.  carry  flag  set)
+  mov dh, 1            ; Restore  DX from  the  stack
+  cmp dh, al       ; if AL (sectors  read) != DH (sectors  expected)
+  jne  disk_error   ;    display  error  message
+
+  mov ah, 0x0e
+  mov al, 48
   int 0x10
-  jmp $           ; Jump to the  current  address (i.e.  forever).
+
+  jmp $
+
+disk_error :
+  mov ah, 0x0e
+  mov al, 51
+  int 0x10
+  ;jmp $
+
 ;
 ; Padding  and  magic  BIOS  number.
 ;
