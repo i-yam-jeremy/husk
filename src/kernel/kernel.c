94fb72e7e1acaff74e110debf3cad21902a055b6
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2019-01-17 15:17:40 -0800

    Draws number char images on screen when keys are pressed

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -4,6 +4,80 @@
 unsigned  char  port_byte_in(unsigned  short  port);
 void  port_byte_out(unsigned  short  port , unsigned  char  data);
 
+void render_1bit_image(unsigned char *screen, unsigned char *image, int width, int height, int x, int y, unsigned char color);
+
+unsigned char font[] = {
+
+  0, 1, 1, 0,
+  1, 0, 0, 1,
+  1, 0, 0, 1,
+  1, 0, 0, 1,
+  0, 1, 1, 0,
+
+
+  0, 1, 1, 0,
+  0, 0, 1, 0,
+  0, 0, 1, 0,
+  0, 0, 1, 0,
+  0, 1, 1, 1,
+
+
+  0, 1, 1, 0,
+  0, 0, 1, 0,
+  0, 1, 0, 0,
+  0, 1, 0, 0,
+  0, 1, 1, 0,
+
+
+  0, 1, 1, 0,
+  0, 0, 1, 0,
+  0, 1, 1, 0,
+  0, 0, 1, 0,
+  0, 1, 1, 0,
+
+
+  1, 0, 1, 0,
+  1, 0, 1, 0,
+  1, 1, 1, 1,
+  0, 0, 1, 0,
+  0, 0, 1, 0,
+
+
+  0, 1, 1, 0,
+  0, 1, 0, 0,
+  0, 0, 1, 0,
+  0, 0, 1, 0,
+  0, 1, 1, 0,
+
+
+  0, 0, 1, 0,
+  0, 1, 0, 0,
+  0, 1, 1, 0,
+  0, 1, 0, 1,
+  0, 0, 1, 0,
+
+
+  0, 1, 1, 1,
+  0, 0, 0, 1,
+  0, 0, 1, 0,
+  0, 0, 1, 0,
+  0, 1, 0, 0,
+
+
+  0, 1, 1, 0,
+  1, 0, 0, 1,
+  0, 1, 1, 0,
+  1, 0, 0, 1,
+  0, 1, 1, 0,
+
+
+  0, 1, 1, 1,
+  0, 1, 0, 1,
+  0, 1, 1, 1,
+  0, 0, 0, 1,
+  0, 1, 1, 0
+};
+
 void main() {
   unsigned char *screen = (unsigned char *) 0xA0000;
 
@@ -13,17 +87,25 @@ void main() {
     unsigned char b = port_byte_in(0x60);// | port_byte_in(0x61) | port_byte_in(0x62) | port_byte_in(0x63) | port_byte_in(0x64);
 
     if (1 <= b && b <= 10) {
-      for (int y = 0; y < HEIGHT; y++) {
-        for (int x = 0; x < WIDTH; x++) {
-          screen[y*WIDTH + x] = b;
-        }
-      }
+      render_1bit_image(screen, &(font[20*(b-1)]), 4, 5, 25, 25, 0x0F);
     }
     else {
       for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
           screen[y*WIDTH + x] = 0x00;
         }
+      }
+    }
+  }
+}
+
+void render_1bit_image(unsigned char *screen, unsigned char *image, int width, int height, int x, int y, unsigned char color) {
+  for (int iy = 0; iy < height; iy++) { // iy = iterator y
+    for (int ix = 0; ix < width; ix++) { // ix = iterator x
+      int sx = x+ix; // sx = screen x
+      int sy = y+iy; // sy = screen y
+      if (0 <= sx && sx < WIDTH && 0 <= sy && sy < HEIGHT) {
+        screen[sy*WIDTH + sx] = ((image[iy*width + ix] == 1) ? color : 0x00);
       }
     }
   }
